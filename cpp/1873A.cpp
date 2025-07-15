792a15482ee75124cfc428b08ec652f3a3bff743
Ameed Darawsheh <ce.ameed@gmail.com>
2025-07-15 09:00:17 +0300

    Refactor solution for problem 1873A: simplify string comparison logic

--- cpp/1873A.cpp
+++ cpp/1873A.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
+#define endl '\n'
 
 int main() {
   ios::sync_with_stdio(0);
@@ -8,13 +10,27 @@ int main() {
   int t;
   cin >> t;
   while (t--) {
+
+    bool able = false;
     string s;
     cin >> s;
-
-    if (s == "abc" || s == "acb" || s == "bac") {
-      cout << "YES\n";
+    if (s == "abc") {
+      cout << "YES" << endl;
+      continue;
+    }
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        swap(s[i], s[j]);
+        if (s == "abc") {
+          able = true;
+        }
+        swap(s[i], s[j]);
+      }
+    }
+    if (able) {
+      cout << "YES" << endl;
     } else {
-      cout << "NO\n";
+      cout << "NO" << endl;
     }
   }
 
