ba8a3133968551e9b30634c0d96b40bfbb315545
Ameed Darawsheh <ce.ameed@gmail.com>
2025-07-18 00:40:14 +0300

    Add solutions for problems 1950A and 2095J: implement logic for identifying stair, peak, and output based on input string

---
+++ cpp/2095J.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define endl '\n'
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+  if (s == "first")
+    cout << "Nauuo and Votes";
+  else
+    cout << "Nauuo and Chess";
+
+  return 0;
+}
