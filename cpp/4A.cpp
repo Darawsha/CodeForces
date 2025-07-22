002966e30a444038e7c3818073e4a10c78377bb5
Ameed Darawsheh <ce.ameed@gmail.com>
2025-07-22 22:52:57 +0300

    Add solutions for problems 4A and 71A: implement logic for even number check and string abbreviation

---
+++ cpp/4A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define endl '\n'
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t;
+  cin >> t;
+  if (t % 2 == 0 and t > 2 )
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+
+  return 0;
+}
