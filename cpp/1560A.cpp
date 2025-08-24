98eace8559bd4ac3be9f4805157bda7f36ae8501
Ameed Darawsheh <s12240153@stu.najah.edu>
2025-08-25 00:19:15 +0300

    Refactor solution for problem 1560A: streamline input handling and improve code structure

--- cpp/1560A.cpp
+++ cpp/1560A.cpp
@@ -1,34 +1,35 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-// #include <bits/stdc++.h>   use this line if you want to include all standard libraries
+#include <iostream>
+#include <string>
+#include <vector>
+#define ll long long
+#define vi vector<int>
+#define str string
 using namespace std;
+#define endl '\n'
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int t = 0;
-    cin >> t;
-    vector<int> a(t);
-    for (int i = 0; i < t; i++)
-    {
-        cin >> a[i];
-        int count = 0, num = 1;
-        while (true)
-        {
-            if (num % 3 != 0 && num % 10 != 3)
-            {
-                count++;
-                if (count == a[i])
-                {
-                    cout << num << '\n';
-                    break;
-                }
-            }
-            num++;
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("../0.3", "r", stdin);
+#endif
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int count = 0, num = 1;
+    while (true) {
+      if (num % 3 != 0 and num % 10 != 3) {
+        count++;
+        if (count == n) {
+          cout << num << endl;
+          break;
         }
+      }
+      num++;
     }
-    return 0;
+  }
+  return 0;
 }
