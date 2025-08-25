f69a500c42f94b3f5e8f9cd57e2012e2b09f1de9
Ameed Darawsheh <s12240153@stu.najah.edu>
2025-08-25 18:52:11 +0300

    Refactor code formatting in solution for problem 472A: improve readability and consistency

--- cpp/472A.cpp
+++ cpp/472A.cpp
@@ -1,32 +1,39 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
+#define ll long long
+#define vi vector<int>
+#define str string
 using namespace std;
-
-bool isComposite(int num)
-{
-    if (num < 4)
-        return false;
-    for (int i = 2; i * i <= num; ++i)
-    {
-        if (num % i == 0)
-            return true;
-    }
+#define endl '\n'
+bool isComposite(int num) {
+  if (num < 4)
     return false;
+  for (int i = 2; i * i <= num; ++i) {
+    if (num % i == 0)
+      return true;
+  }
+  return false;
 }
 
-int main()
-{
-    int n;
-    cin >> n;
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("../0.3", "r", stdin);
+#endif
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    for (int x = 4; x < n; ++x)
-    {
-        int y = n - x;
-        if (isComposite(x) && isComposite(y))
-        {
-            cout << x << " " << y << '\n';
-            break;
-        }
+  int n;
+  cin >> n;
+
+  for (int x = 4; x < n; ++x) {
+    int y = n - x;
+    if (isComposite(x) && isComposite(y)) {
+      cout << x << " " << y << '\n';
+      break;
     }
+  }
 
-    return 0;
+  return 0;
 }
