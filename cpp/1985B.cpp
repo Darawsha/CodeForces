67b16c4e5c8f47c0103ae514d08bb40e1ca939fe
Ameed Darawsheh <ce.ameed@gmail.com>
2025-08-30 23:39:37 +0300

    Add solution for problem 1985B: implement main logic and input handling

---
+++ cpp/1985B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+#define endl '\n'
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("../0.3", "r", stdin);
+  freopen("../0.3res", "w", stdout);
+#endif
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    if (n == 2 or n == 3) {
+      cout << n << endl;
+
+    } else
+      cout << 2 << endl;
+  }
+
+  return 0;
+}
