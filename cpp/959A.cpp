4331d4f00adb739d9ceeee4f460cd0ea3b2f5598
Ameed Darawsheh <ce.ameed@gmail.com>
2025-08-31 18:03:31 +0300

    Add solution for problem 959A: implement main logic and input handling

---
+++ cpp/959A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+#define endl '\n'
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("../0.3", "r", stdin);
+  freopen("../0.3res", "w", stdout);
+#endif
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  ll n;
+  cin >> n;
+  cout << ((n % 2 == 0) ? "Mahmoud" : "Ehab") << endl;
+
+  return 0;
+}
