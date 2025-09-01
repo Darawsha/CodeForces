1949f4061aec42b0fbdd8b0d462430d6ae660c18
Ameed Darawsheh <ce.ameed@gmail.com>
2025-09-01 22:28:40 +0300

    Add solution for problem 2095D: implement main logic and input handling

---
+++ cpp/2095D.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+#define endl '\n'
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("../0.3", "r", stdin);
+  freopen("../0.3res", "w", stdout);
+#endif
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cout << "36.104300 -115.172835" << endl;
+
+  return 0;
+}
