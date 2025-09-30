48b7f5dbb7554b8718f9b0bc08cbe1ab06d69a66
Ameed Darawsheh <ce.ameed@gmail.com>
2025-09-30 20:50:39 +0300

    Add solutions for problems 1878C and 1881A: implement main logic and input handling

---
+++ cpp/1881A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+
+void fast_io() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+}
+
+void file_io() {
+#ifndef ONLINE_JUDGE
+  freopen("../0.3", "r", stdin);
+  freopen("../0.3res", "w", stdout);
+#endif
+}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  string x, s;
+  cin >> x >> s;
+  int res = -1;
+  for (int i = 0; i <= 6; i++) {
+    if (x.find(s) != string::npos) {
+      res = i;
+      break;
+    }
+    x += x;
+  }
+  cout << res << endl;
+}
+
+int main() {
+  file_io();
+  fast_io();
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
